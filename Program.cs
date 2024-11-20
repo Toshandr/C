0541b0e209938f7d824bf2a9ba6353c6253fbb89
Антон <antonnechlopochin@mail.ru>
2024-11-20 19:47:35 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace C;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int a = Convert.ToInt16(Console.ReadLine());
+        int b = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine(a + b);
+        Console.WriteLine(a - b);
+        Console.WriteLine(a * b);
+        Console.WriteLine(a / b);
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,10 @@
+namespace C;
+
+class Programm
+{
+    static void Master(string[] args)
+    {
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine(a + 5);
+    }
+}
